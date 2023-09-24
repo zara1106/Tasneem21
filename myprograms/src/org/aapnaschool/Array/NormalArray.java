af62a4dd5ff95568173668c1ff943c6e6903d4f4
irfan <irfan@LAPTOP-31BTN3JE>
2023-09-24 09:22:17 +0530

    first commit

---
+++ myprograms/src/org/aapnaschool/Array/NormalArray.java
@@ -0,0 +1,17 @@
+package org.aapnaschool.Array;
+//the program is demonstrate the array
+public class NormalArray {
+//
+	public static void main(String[] args) {
+		int[] marks=new int[3];
+		//storing the number of student marks in one array
+		//[] square bracket is use to store the Array element 
+		marks[0]=78;//physics
+		marks[1]=89;//mathematics
+		marks[2]=90;//biology
+		System.out.println("the physics marks is->" + marks[0]);
+		System.out.println("the maths marks is->" + marks[1]);
+		System.out.println("the biology marks is->" + marks[2]);
+	}
+
+}
